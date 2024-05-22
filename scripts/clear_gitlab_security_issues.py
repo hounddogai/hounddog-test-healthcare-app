367de455da21f3570e43469fd4004a7676e5f388
Joohwan Oh <joohwan.oh@outlook.com>
2024-05-22 02:29:30 -0700

    Iterate on pipeline files

--- scripts/clear_gitlab_security_issues.py
+++ scripts/clear_gitlab_security_issues.py
@@ -6,21 +6,84 @@ import sys
 import httpx
 
 
-if __name__ == "__main__":
-    http = httpx.Client(
-        base_url="https://gitlab.com/api/v4",
-        headers={"Private-Token": os.environ["GITLAB_TOKEN"]},
+def run_graphql_query(query):
+    response = httpx.post(
+        url="https://gitlab.com/api/graphql",
+        headers={
+            "Authorization": f"Bearer {os.environ['GITLAB_TOKEN']}",
+            "Content-Type": "application/json",
+        },
+        json={"query": query},
+        timeout=30,
     )
-    resp = http.get(f"/projects/{os.environ['CI_PROJECT_ID']}/vulnerabilities")
+    if not response.is_success:
+        sys.exit(f"Error executing query: {response.status_code} {response.text}")
+    return response.json()
 
-    if not resp.is_success:
-        sys.exit(f"Failed to get security issues: {resp.status_code} {resp.text}")
 
-    security_issues = resp.json()
-    print(f"Found {len(security_issues)} security issues")
+def get_vulnerabilities():
+    """Retrieve all open vulnerabilities for a given project."""
+    query = """
+    query {
+      project(fullPath: "hounddog-ai/hounddog-test-healthcare-app") {
+        vulnerabilities(state: [DETECTED, CONFIRMED]) {
+          nodes { id }
+        }
+      }  
+    }
+    """
+    result = run_graphql_query(query)
+    print(result)
 
-    for issue in security_issues:
-        print(f"Resolving security issue {issue['id']} ...")
-        resp = http.delete(f"/vulnerabilities/{issue['id']}/resolve")
-        if resp.status_code not in (201, 304):
-            sys.exit(f"Cannot resolve issue {issue['id']}: {resp.status_code} {resp.text}")
+
+def resolve_vulnerability(vulnerability_id):
+    mutation = """
+    mutation {
+      vulnerabilityResolve(input: { id: "gid://gitlab/Vulnerability/23577695"}) {
+        vulnerability {
+          state
+        }
+        errors
+      }
+    }
+    """
+
+    """Close a vulnerability by its ID."""
+    mutation = f"""
+    mutation {{
+      vulnerabilityDismiss(input: {{ id: "{vulnerability_id}", comment: "Closed by automated script" }}) {{
+        errors
+        vulnerability {{
+          id
+          title
+          state
+        }}
+      }}
+    }}
+    """
+    result = run_graphql_query(mutation)
+    if result:
+        return result["data"]["vulnerabilityDismiss"]["vulnerability"]
+    return None
+
+
+if __name__ == "__main__":
+    get_vulnerabilities()
+
+    # resp = http.get(f"/projects/{os.environ['CI_PROJECT_ID']}/vulnerabilities")
+    #
+    # if not resp.is_success:
+    #     sys.exit(f"Failed to get security issues: {resp.status_code} {resp.text}")
+    #
+    # security_issues = resp.json()
+    # print(
+    #     f"Found {len(security_issues)} security issues in project {os.environ['CI_PROJECT_ID']}"
+    # )
+    #
+    # for issue in security_issues:
+    #     print(f"Resolving security issue {issue['id']} ...")
+    #     resp = http.delete(f"/vulnerabilities/{issue['id']}/resolve")
+    #     if resp.status_code not in (201, 304):
+    #         sys.exit(
+    #             f"Cannot resolve issue {issue['id']}: {resp.status_code} {resp.text}"
+    #         )
